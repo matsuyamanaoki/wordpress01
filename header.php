d83cc89fb6ac41070ad3770766d461d9120c7c74
matsuyamanaoki <mountain.30hope@docomo.ne.jp>
2021-06-17 21:13:03 +0900

    メニュー関係

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 			<?php
 			$custom_logo_id = get_theme_mod( 'custom_logo' );
 			$image          = wp_get_attachment_image_src( $custom_logo_id, 'full' );
-			
+
 			$html  = '<img src="' . $image[0] . '"';
 			$html .= ' width="' . $image[1] . '"';
 			$html .= ' height="' . $image[2] . '"';
@@ -27,37 +27,27 @@
 				<label id="gnav_open" class="gnav-open" for="gnav_input"><span></span></label>
 				<label id="gnav_close" class="gnav-close" for="gnav_input"></label>
 				<div id="gnav-content" class="gnav-content">
-					<ul class="gnav-menu">
-						<li>
-							<a href="#service">Service<br>サービス</a>
-						</li>
-						<li>
-							<a href="#plan">Plan<br>プラン</a>
-						</li>
-						<li>
-							<a href="#access">Access<br>アクセス</a>
-						</li>
-						<li>
-							<a href="#contact">Contact<br>コンタクト</a>
-						</li>
-					</ul>
+					<?php
+					if ( has_nav_menu( 'global' ) ) {
+						wp_nav_menu( array(
+							'thme_location' => 'global',
+							'container'     => '',
+							'items_wrap'    => '<ul class="gnav-menu">%3$s</ul>'
+						) );
+					}
+					?>
 				</div>
 			</div>
 		</nav>
 		<nav class="pc-only">
-			<ul class="gnav-menu">
-				<li>
-					<a href="#service">Service<br>サービス</a>
-				</li>
-				<li>
-					<a href="#plan">Plan<br>プラン</a>
-				</li>
-				<li>
-					<a href="#access">Access<br>アクセス</a>
-				</li>
-				<li>
-					<a href="#contact">Contact<br>コンタクト</a>
-				</li>
-			</ul>
+			<?php
+			if ( has_nav_menu( 'global' ) ) {
+				wp_nav_menu( array(
+					'thme_location' => 'global',
+					'container'     => '',
+					'items_wrap'    => '<ul class="gnav-menu">%3$s</ul>'
+				) );
+			}
+			?>
 		</nav>
 	</header>
